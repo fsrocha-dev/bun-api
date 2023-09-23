19d72b07b0b1ba3849fc5ccbe64f53f970af879d
Frank Rocha <contato.frankrocha@gmail.com>
2023-09-23 18:11:15 -0300

    refactor: convert user interface in class

--- src/core/user/models/User.ts
+++ src/core/user/models/User.ts
@@ -1,8 +1,17 @@
-export default interface User {
-	id?: number;
+import UuidGenerator from "../utils/UuidGenerator";
+
+export default class User {
+	id?: string;
 	name: string;
 	email: string;
 	password: string;
 	created_at?: Date;
 	updated_at?: Date;
+
+	constructor(name: string, email: string, password: string) {
+    this.id = new UuidGenerator().generate();
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
 }
