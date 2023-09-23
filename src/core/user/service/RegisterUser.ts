682eb42565c488bc7d7d708cba9158fa5c5e7b7b
Frank Rocha <contato.frankrocha@gmail.com>
2023-09-23 18:11:37 -0300

    feat: implements execute register user

--- src/core/user/service/RegisterUser.ts
+++ src/core/user/service/RegisterUser.ts
@@ -9,10 +9,21 @@ type RegisterUserRequest = {
 }
 
 export default class RegisterUser implements UseCase<RegisterUserRequest, void> {
-
 	constructor(private readonly repository: RepositoryUser) {}
 
-	execute(input: RegisterUserRequest): Promise<void> {
+	async execute(input: RegisterUserRequest): Promise<void> {
 		const { name, email, password, confirmPassword } = input;
+
+		if (password !== confirmPassword) {
+			throw new Error("Password and confirm password must be the same");
+		}
+
+		const checkUserExists = await this.repository.findByEmail(email);
+
+		if (checkUserExists) {
+			throw new Error("User already exists");
+		}
+
+		await this.repository.create({ name, email, password });
 	}
 }
